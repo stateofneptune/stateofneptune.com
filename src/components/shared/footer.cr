3dac92b227a85885cae1bb87698820c994fce934
Elia Franzella <eliafranzella@live.it>
2023-04-12 01:23:20 +0200

    refactor: Don't mount nav bar

--- src/components/shared/footer.cr
+++ src/components/shared/footer.cr
@@ -2,9 +2,37 @@ class Shared::Footer < BaseComponent
   def render
     footer class: "font-text-body" do
       section class: "p-2" do
-        mount NavBar, elements: NAV_ELEMENTS
-
+        render_navigation_bar
         render_copyright
+      end
+    end
+  end
+
+  def render_navigation_bar
+    nav do
+      ul class: "flex justify-center items-center" do
+        render_navigation_bar_elements
+      end
+    end
+  end
+
+  private NAV_ELEMENTS = [
+    {"https://www.instagram.com/stateofneptune/", "fab fa-instagram"},
+    {"https://www.facebook.com/stateofneptune/", "fab fa-facebook"},
+    {"https://twitter.com/stateofneptune", "fab fa-twitter"},
+    {"https://www.youtube.com/@stateofneptune1864", "fab fa-youtube"},
+    {"https://open.spotify.com/artist/5FhQX1j6F5axsMOc1UiDmW", "fab fa-spotify"},
+    {"https://stateofneptune.bandcamp.com/", "fab fa-bandcamp"},
+    {"https://www.deezer.com/us/artist/158840862", "fab fa-deezer"},
+    {"mailto:stateofneptune@gmail.com", "fas fa-envelope"},
+  ]
+
+  def render_navigation_bar_elements
+    NAV_ELEMENTS.each do |href, fa_class|
+      div class: "h-12 w-12 min-w-[2rem] transition-all flex justify-center items-center" do
+        a class: "text-md md:text-lg transition-all hover:text-accent", href: href, target: "_blank" do
+          i class: fa_class
+        end
       end
     end
   end
@@ -12,47 +40,6 @@ class Shared::Footer < BaseComponent
   def render_copyright
     div class: "flex text-center justify-center" do
       text "© 2023 State Of Neptune – All Rights Reserved"
-    end
-  end
-
-  private NAV_ELEMENTS = [
-    NavBar::Element.new("https://www.instagram.com/stateofneptune/", "fab fa-instagram"),
-    NavBar::Element.new("https://www.facebook.com/stateofneptune/", "fab fa-facebook"),
-    NavBar::Element.new("https://twitter.com/stateofneptune", "fab fa-twitter"),
-    NavBar::Element.new("https://stateofneptune.bandcamp.com/", "fab fa-bandcamp"),
-    NavBar::Element.new("https://open.spotify.com/artist/5FhQX1j6F5axsMOc1UiDmW", "fab fa-spotify"),
-    NavBar::Element.new("https://www.deezer.com/us/artist/158840862", "fab fa-deezer"),
-    NavBar::Element.new("https://www.youtube.com/@stateofneptune1864", "fab fa-youtube"),
-    NavBar::Element.new("mailto:stateofneptune@gmail.com", "fas fa-envelope"),
-  ]
-
-  class NavBar < BaseComponent
-    needs elements : Array(Element)
-
-    class Element
-      getter href : String
-      getter fa_class : String
-
-      def initialize(@href : String, @fa_class : String)
-      end
-    end
-
-    def render
-      nav do
-        ul class: "flex justify-center items-center" do
-          elements.each do |element|
-            render_element element
-          end
-        end
-      end
-    end
-
-    def render_element(element : Element)
-      div class: "h-12 w-12 min-w-[2rem] flex justify-center items-center" do
-        a class: "text-md md:text-lg transition-all hover:text-persian-blue", href: element.href, target: "_blank" do
-          i class: element.fa_class
-        end
-      end
     end
   end
 end
