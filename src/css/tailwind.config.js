4e67c5b0fc09a3be2c1fd7e192e6bff2c7e2cfa3
Elia Franzella <eliafranzella@live.it>
2023-05-06 18:06:29 +0200

    feat: Add fade in/out animations to tw config

--- src/css/tailwind.config.js
+++ src/css/tailwind.config.js
@@ -21,6 +21,26 @@ module.exports = {
         'dim': "#B3AEA4"
       },
     },
+    keyframes: {
+      'fade-in': {
+        '0%': { visibility: 'hidden', opacity: 0 },
+        '100%': { visibility: 'visible', opacity: 1 },
+      },
+      'fade-out': {
+        '0%': { visibility: 'visible', opacity: 1 },
+        '100%': { visibility: 'hidden', opacity: 0 },
+      },
+    },
+    animation: {
+      'fade-in-slowest': 'fade-in 1000ms ease-out forwards',
+      'fade-in-slower': 'fade-in 500ms ease-out forwards',
+      'fade-in-slow': 'fade-in 300ms ease-out forwards',
+      'fade-in': 'fade-in 150ms ease-out forwards',
+      'fade-out-slowest': 'fade-out 1000ms ease-out forwards',
+      'fade-out-slower': 'fade-out 500ms ease-out forwards',
+      'fade-out-slow': 'fade-out 300ms ease-out forwards',
+      'fade-out': 'fade-out 150ms ease-out forwards',
+    },
   },
   plugins: [],
 }
