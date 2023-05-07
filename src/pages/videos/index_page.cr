e3fb2d6b3d68632d75712812ed4d6cb7f8098064
Elia Franzella <eliafranzella@live.it>
2023-05-07 02:02:33 +0200

    feat: Add Umbilical video

--- src/pages/videos/index_page.cr
+++ src/pages/videos/index_page.cr
@@ -4,5 +4,16 @@ class Videos::IndexPage < MainLayout
   end
 
   def content
+    section class: "p-16" do
+      div class: "flex justify-center" do
+        render_embedded_yt_video src: "https://www.youtube-nocookie.com/embed/Ff3ebB6Un8M"
+      end
+    end
+  end
+
+  def render_embedded_yt_video(src : String)
+    iframe width: 560, height: 315, src: src, title: "YouTube video player", frameborder: 0,
+      allow: "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share",
+      attrs: [:allowfullscreen]
   end
 end
