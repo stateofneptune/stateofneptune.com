229c63e0aa1faebfd1188c2c400430ca1da4d3df
Elia Franzella <eliafranzella@live.it>
2023-05-23 20:42:22 +0200

    refactor: Rename youtube embed component

--- src/routes/videos/index.tsx
+++ src/routes/videos/index.tsx
@@ -1,14 +1,14 @@
 import { component$ } from "@builder.io/qwik";
 import { DocumentHead } from "@builder.io/qwik-city";
 
-import YoutubeEmbed from "~/components/youtube-embed/youtube-embed";
+import YouTube from "~/components/you-tube/you-tube";
 
 export default component$(() => {
   return (
     <>
       <section class="flex w-full justify-center">
         <div class="flex w-[80rem] flex-wrap justify-evenly gap-x-16 gap-y-16">
-          <YoutubeEmbed src="https://www.youtube-nocookie.com/embed/Ff3ebB6Un8M" />
+          <YouTube src="https://www.youtube-nocookie.com/embed/Ff3ebB6Un8M" />
         </div>
       </section>
     </>
