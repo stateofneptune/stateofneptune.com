4db1c6e5f59cf8d334fe6258a3d25d10e9412423
Elia Franzella <eliafranzella@live.it>
2023-05-28 02:50:16 +0200

    chore: Remove unneeded @ts-ignore

--- src/components/you-tube/you-tube.tsx
+++ src/components/you-tube/you-tube.tsx
@@ -3,16 +3,13 @@ import { component$ } from "@builder.io/qwik";
 export default component$(({ src }: { src: string }) => {
   return (
     <>
-      {
-        /* @ts-ignore */
-        <iframe
-          class="aspect-video basis-[36rem]"
-          src={src}
-          title="YouTube video player"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowFullScreen
-        ></iframe>
-      }
+      <iframe
+        class="aspect-video basis-[36rem]"
+        src={src}
+        title="YouTube video player"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowFullScreen
+      ></iframe>
     </>
   );
 });
