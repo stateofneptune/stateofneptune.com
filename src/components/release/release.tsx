53de2d34becd8313eaf01aa2e95dd5f2a5d56783
Elia Franzella <eliafranzella@live.it>
2023-05-30 02:18:12 +0200

    feat: Add releases to homepage

---
+++ src/components/release/release.tsx
@@ -0,0 +1,43 @@
+import { ClassList, Signal, component$ } from "@builder.io/qwik";
+
+export interface ReleaseProps {
+  id?: string;
+  heading: string;
+  class?: ClassList | Signal<ClassList>;
+  href: string;
+  src: string;
+  alt?: string;
+}
+
+export default component$((props: ReleaseProps) => {
+  return (
+    <>
+      <article
+        id={props.id}
+        class="flex min-h-[80lvh] flex-wrap content-center items-center justify-center gap-y-12"
+      >
+        <figure class="min-w-[14rem] basis-96">
+          <a href={props.href} target="_blank">
+            <img
+              src={props.src}
+              alt={props.alt}
+              class="shadow-lg transition-[box-shadow] hover:shadow-2xl"
+            ></img>
+          </a>
+        </figure>
+
+        <div class="flex basis-96 flex-col items-center justify-center gap-y-2 transition-[gap] md:gap-y-4">
+          <div class="text-center font-subtitle text-lg uppercase text-accent transition-[font-size] md:text-xl">
+            {props.heading}
+          </div>
+
+          <div class="text-center font-subtitle uppercase transition-[color] hover:text-accent">
+            <a href={props.href} target="_blank">
+              Buy
+            </a>
+          </div>
+        </div>
+      </article>
+    </>
+  );
+});
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,35 @@
 import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 
+import Release from "~/components/release/release";
+
 export default component$(() => {
-  return <></>;
+  return (
+    <>
+      <section>
+        <Release
+          id="pulp-of-stones"
+          heading="Album out now"
+          src="images/artworks/pulp-of-stones@640.webp"
+          href="https://stateofneptune.bandcamp.com/album/pulp-of-stones"
+        ></Release>
+
+        <Release
+          id="paradox"
+          heading="Single out now"
+          src="images/artworks/paradox@640.webp"
+          href="https://stateofneptune.bandcamp.com/track/paradox"
+        ></Release>
+
+        <Release
+          id="fury"
+          heading="Single out now"
+          src="images/artworks/fury@640.webp"
+          href="https://stateofneptune.bandcamp.com/track/fury"
+        ></Release>
+      </section>
+    </>
+  );
 });
 
 export const head: DocumentHead = {
