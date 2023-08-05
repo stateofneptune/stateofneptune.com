acea517109aaacd08be4e4a63f19d2ea6ce2022c
Elia Franzella <eliafranzella@live.it>
2023-08-05 02:41:57 +0200

    feat: Add Stone Lady Live embedded video

--- src/routes/videos/index.tsx
+++ src/routes/videos/index.tsx
@@ -8,6 +8,7 @@ export default component$(() => {
     <>
       <section class="flex w-full justify-center">
         <div class="flex w-[80rem] flex-wrap justify-evenly gap-x-16 gap-y-16">
+          <YouTube src="https://www.youtube-nocookie.com/embed/u7uWaHhZ_Yk" />
           <YouTube src="https://www.youtube-nocookie.com/embed/n-qIKsquRik" />
           <YouTube src="https://www.youtube-nocookie.com/embed/Ff3ebB6Un8M" />
         </div>
