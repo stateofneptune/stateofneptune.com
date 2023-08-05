2f2f8fa3e32169ee09cb069bba79aa87619f4ad1
Elia Franzella <eliafranzella@live.it>
2023-08-05 03:13:18 +0200

    refactor: Use flex instead of margin to distribute sections

--- src/routes/privacy-policy/index.tsx
+++ src/routes/privacy-policy/index.tsx
@@ -9,92 +9,73 @@ export default component$(() => {
   return (
     <>
       <section class="flex w-full justify-center">
-        <div class="w-[80ch]">
-          <div class="mb-6">
-            <Title>Privacy Policy</Title>
-          </div>
+        <div class="flex w-[80ch] flex-col gap-6">
+          <Title>Privacy Policy</Title>
 
-          <div class="mb-6">
-            <Paragraph>
-              This Policy explains how your personal data may be processed when
-              you visit and browse our website and social media channels.
-            </Paragraph>
-          </div>
+          <Paragraph>
+            This Policy explains how your personal data may be processed when
+            you visit and browse our website and social media channels.
+          </Paragraph>
 
-          <div class="mb-6">
-            <Heading>We don't collect any personal data</Heading>
-          </div>
-          <div class="mb-6">
-            <Paragraph>
-              This website (
-              <Link
-                class="decoration-accent underline-offset-2 focus-within:underline hover:underline"
-                href="/"
-              >
-                www.stateofneptune.com
-              </Link>
-              ) does not collect any personal data. Our definition of "personal
-              data" is based on the privacy laws and regulation of the EU,
-              including the General Data Protection Regulation (GDPR). We
-              consider any information about you or your behaviour that can be
-              tracked back as personal data. Our only interests are informing
-              you about our content and enabling you to contact us. Therefore:
-            </Paragraph>
+          <Heading>We don't collect any personal data</Heading>
 
-            <ul class="list-inside list-[square] font-body text-lg">
-              <li>
-                We do not set any cookies at all on this site or your own
-                device.
-              </li>
-              <li>
-                We do not collect any data whatsoever about people who visit our
-                site.
-              </li>
-              <li>
-                We do not conduct any analysis of how people use our site.
-              </li>
-              <li>
-                We do not allow third-party tracking of any kind on this site.
-              </li>
-            </ul>
-          </div>
+          <Paragraph>
+            This website (
+            <Link
+              class="decoration-accent underline-offset-2 focus-within:underline hover:underline"
+              href="/"
+            >
+              www.stateofneptune.com
+            </Link>
+            ) does not collect any personal data. Our definition of "personal
+            data" is based on the privacy laws and regulation of the EU,
+            including the General Data Protection Regulation (GDPR). We consider
+            any information about you or your behaviour that can be tracked back
+            as personal data. Our only interests are informing you about our
+            content and enabling you to contact us.
+            <br />
+            Therefore:
+          </Paragraph>
 
-          <div class="mb-6">
-            <Heading>
-              We don't disclose or sell your contact information
-            </Heading>
-          </div>
-          <div class="mb-6">
-            <Paragraph>
-              When you contact us via e-mail, we'll use your contact information
-              to contact you. We won't share (nor sell) this information to
-              anyone else.
-            </Paragraph>
-          </div>
+          <ul class="list-inside list-[square] font-body text-lg">
+            <li>
+              We do not set any cookies at all on this site or your own device.
+            </li>
+            <li>
+              We do not collect any data whatsoever about people who visit our
+              site.
+            </li>
+            <li>We do not conduct any analysis of how people use our site.</li>
+            <li>
+              We do not allow third-party tracking of any kind on this site.
+            </li>
+          </ul>
 
-          <div class="mb-6">
-            <Heading>Third-party data processing and retention</Heading>
-          </div>
-          <div class="mb-6">
-            <Paragraph>
-              We selected Cloudflare Pages as our host service. In order to
-              provide this service, Cloudflare processes the Internet Protocol
-              (IP) address of the device accessing the site and other traffic
-              data including the browser software and the operating system in
-              use. This information is processed for the purpose of diagnosing
-              technical problems and defending against attacks.
-            </Paragraph>
-          </div>
+          <Heading>We don't disclose or sell your contact information</Heading>
 
-          <div class="mb-6">
-            <Heading>Changes and revisions</Heading>
-          </div>
-          <div class="mb-6">
-            <Paragraph>
-              We may update this Policy in the future. We will track such
-              changes in this section.
-            </Paragraph>
-          </div>
+          <Paragraph>
+            When you contact us via e-mail, we'll use your contact information
+            to contact you. We won't share (nor sell) this information to anyone
+            else.
+          </Paragraph>
+
+          <Heading>Third-party data processing and retention</Heading>
+
+          <Paragraph>
+            We selected Cloudflare Pages as our host service. In order to
+            provide this service, Cloudflare processes the Internet Protocol
+            (IP) address of the device accessing the site and other traffic data
+            including the browser software and the operating system in use. This
+            information is processed for the purpose of diagnosing technical
+            problems and defending against attacks.
+          </Paragraph>
+
+          <Heading>Changes and revisions</Heading>
+
+          <Paragraph>
+            We may update this Policy in the future. We will track such changes
+            in this section.
+          </Paragraph>
 
           <p class="font-body text-sm text-dim md:text-base">
             Policy published 5th August 2023 – Policy updated 5th August 2023.
