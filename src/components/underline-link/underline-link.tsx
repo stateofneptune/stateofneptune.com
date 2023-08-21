7025a021f77966242532d95ab4b6b966e7b1604a
Elia Franzella <eliafranzella@live.it>
2023-08-21 02:37:02 +0200

    feat: Add external link support for underline link component

--- src/components/underline-link/underline-link.tsx
+++ src/components/underline-link/underline-link.tsx
@@ -4,19 +4,32 @@ import { Link } from "@builder.io/qwik-city";
 export interface UnderlineLinkProps {
   class?: ClassList;
   href?: string;
+  external?: boolean;
 }
 
 export default component$((props: UnderlineLinkProps) => {
   return (
     <>
-      <Link
-        class="font-body decoration-accent underline-offset-2 focus-within:underline hover:underline"
-        href={props.href}
-      >
-        <span class={props.class}>
-          <Slot />
-        </span>
-      </Link>
+      {props.external ? (
+        <a
+          class="font-body decoration-accent underline-offset-2 focus-within:underline hover:underline"
+          href={props.href}
+          target="_blank"
+        >
+          <span class={props.class}>
+            <Slot />
+          </span>
+        </a>
+      ) : (
+        <Link
+          class="font-body decoration-accent underline-offset-2 focus-within:underline hover:underline"
+          href={props.href}
+        >
+          <span class={props.class}>
+            <Slot />
+          </span>
+        </Link>
+      )}
     </>
   );
 });
