2273d0f66937eca20b09425cfdc7aa1de141daab
Elia Franzella <eliafranzella@live.it>
2023-08-24 22:48:05 +0200

    feat: Add title to route `/videos`

--- src/routes/videos/index.tsx
+++ src/routes/videos/index.tsx
@@ -1,11 +1,14 @@
 import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 
+import Title from "~/components/title/title";
 import YouTube from "~/components/you-tube/you-tube";
 
 export default component$(() => {
   return (
     <>
+      <Title>Videos</Title>
+
       <section class="flex w-full justify-center">
         <div class="flex w-[80rem] flex-wrap justify-center gap-16">
           <YouTube src="https://www.youtube-nocookie.com/embed/u7uWaHhZ_Yk" />
