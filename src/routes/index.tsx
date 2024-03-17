37dc71dad2e6cb6937425fd531997654d317e901
Elia Franzella <eliafranzella@live.it>
2024-03-17 22:40:41 +0100

    refactor: Rewrite homepage

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,32 +1,37 @@
 import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 
-import Release from "~/components/release/release";
+import StateOfNeptuneLogoHQ from "/public/images/logo/state-of-neptune-wht-inline-hq.png?jsx";
 
 export default component$(() => {
   return (
     <>
-      <section class="my-12 px-6">
-        <Release
-          id="pulp-of-stones"
-          heading="Album out now"
-          src="images/artworks/pulp-of-stones@640.webp"
-          href="https://stateofneptune.bandcamp.com/album/pulp-of-stones"
-        ></Release>
+      <section class="relative flex min-h-[90lvh] justify-center">
+        <div
+          class={[
+            "flex w-[100ch] flex-col justify-center gap-6 text-center",
+            "before:absolute before:inset-0 before:-z-10 before:h-full before:w-full",
+            "before:bg-[linear-gradient(rgba(13,13,13,0.5),rgba(13,13,13,0.5)),url(/images/backgrounds/anomalia-live.webp)]",
+            "before:bg-cover before:bg-center before:content-['']",
+          ]}
+        >
+          <StateOfNeptuneLogoHQ />
 
-        <Release
-          id="paradox"
-          heading="Single out now"
-          src="images/artworks/paradox@640.webp"
-          href="https://stateofneptune.bandcamp.com/track/paradox"
-        ></Release>
-
-        <Release
-          id="fury"
-          heading="Single out now"
-          src="images/artworks/fury@640.webp"
-          href="https://stateofneptune.bandcamp.com/track/fury"
-        ></Release>
+          <p class="px-6 font-body text-sm transition-[font] md:text-base lg:text-lg">
+            Mixing a wide range of influences from the rawness of stoner and
+            punk to the intricacies of metal and grunge, State Of Neptune have
+            shared the beginning of their journey on June 11th 2022, with their
+            self-produced debut album "Pulp Of Stones".
+            <br />
+            <br />
+            Creating quite a buzz in their hometown and its surroundings, thanks
+            to a full year of high-energy live performances and the will to
+            embrace the underground culture, on September 15th 2023 they
+            released the session "Live At Vasto Records", which contains live
+            renditions of "Fury", a beloved song from "Pulp Of Stones", and
+            "Stone Lady", a new song and a glimpse of the future to come.
+          </p>
+        </div>
       </section>
     </>
   );
