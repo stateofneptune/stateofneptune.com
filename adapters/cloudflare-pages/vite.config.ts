8e52b08e934fc25e0c3cd67c6274f8e0ee2582f5
Elia Franzella <eliafranzella@live.it>
2024-03-22 02:33:05 +0100

    fix: Use ssg for sitemap

--- adapters/cloudflare-pages/vite.config.ts
+++ adapters/cloudflare-pages/vite.config.ts
@@ -10,6 +10,12 @@ export default extendConfig(baseConfig, () => {
         input: ["src/entry.cloudflare-pages.tsx", "@qwik-city-plan"],
       },
     },
-    plugins: [cloudflarePagesAdapter()],
+    plugins: [cloudflarePagesAdapter({
+      ssg: {
+        include: ["/", "/discography/", "/tour/", "/videos/"],
+        origin: "https://www.stateofneptune.com",
+        sitemapOutFile: "sitemap.xml"
+      }
+    })],
   };
 });
