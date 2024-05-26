1d4c6c18963a92399c5bae13f5ab1d8a0356f5e1
Elia Franzella <eliafranzella@live.it>
2024-05-26 14:56:07 +0200

    fix: Redirect `/music` to `/discography` for SEO

---
+++ src/routes/music/index.tsx
@@ -0,0 +1,10 @@
+import { component$ } from "@builder.io/qwik";
+import type { RequestEvent } from "@builder.io/qwik-city";
+
+export const onGet = async ({ redirect }: RequestEvent) => {
+  throw redirect(301, "/discography");
+};
+
+export default component$(() => {
+  return <></>;
+});
