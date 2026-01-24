0996b24c6728c5a8d3d5afcb9f51b8f33fdd40d2
Elia Franzella <franzella.elia@gmail.com>
2026-01-24 01:41:10 +0100

    feat: Add new singles to home

--- src/routes/discography/releases.tsx
+++ src/routes/discography/releases.tsx
@@ -1,5 +1,7 @@
 import type { JSXOutput } from "@builder.io/qwik";
 
+import NowIRememberMyFaceAgainArtwork from "/public/images/artworks/now-i-remember-my-face-again.jpg?jsx";
+import KillersplinterArtwork from "/public/images/artworks/killersplinter.png?jsx";
 import PulpOfStonesArtwork from "/public/images/artworks/pulp-of-stones.jpg?jsx";
 
 export type Release = {
@@ -18,6 +20,50 @@ export type Release = {
 };
 
 export const RELEASES: Release[] = [
+  {
+    id: "now-i-remember-my-face-again",
+    image: (
+      <NowIRememberMyFaceAgainArtwork alt="Artwork of studio single Now I Remember My Face Again from State Of Neptune." />
+    ),
+    href: "https://open.spotify.com/album/13cRIsGpfSPB3PM4TpTvfl",
+    label: `Listen to Now I Remember My Face Again on Spotify.`,
+    title: "Now I Remember My Face Again",
+    type: "single",
+    links: [
+      {
+        href: "https://open.spotify.com/album/13cRIsGpfSPB3PM4TpTvfl",
+        label: `Listen to Now I Remember My Face Again on Spotify.`,
+        type: "spotify",
+      },
+      {
+        href: "https://music.apple.com/pl/album/now-i-remember-my-face-again-single/1857928875",
+        label: `Listen to Now I Remember My Face Again on Apple Music.`,
+        type: "apple-music",
+      },
+    ],
+  },
+  {
+    id: "killersplinter",
+    title: "Killersplinter",
+    image: (
+      <KillersplinterArtwork alt="Artwork of studio single Killersplinter from State Of Neptune." />
+    ),
+    href: "https://open.spotify.com/album/2OMOeiW7O5v4ikLeWABxWs",
+    label: "Listen to Killersplinter on Spotify.",
+    type: "single",
+    links: [
+      {
+        href: "https://open.spotify.com/album/2OMOeiW7O5v4ikLeWABxWs",
+        label: "Listen to Killersplinter on Spotify.",
+        type: "spotify",
+      },
+      {
+        href: "https://music.apple.com/pl/album/killersplinter-single/1844327013",
+        label: "Listen to Killersplinter on Apple Music.",
+        type: "apple-music",
+      },
+    ],
+  },
   {
     id: "pulp-of-stones",
     title: "Pulp Of Stones",
